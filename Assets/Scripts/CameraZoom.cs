b71ee62fcc2a3d643c5fff0e1832a7bd0bc7d061
Isabel Paehr <isabel.paehr@gmail.com>
2020-07-01 11:26:08 +0200

    dev: new: camera rotation

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -14,10 +14,20 @@ public class CameraZoom : MonoBehaviour
     // mouse values 
     private float scrollClamp = 5f;
 
+    private Vector3 lastMousePos;
+
     // camera values 
     private Vector3 camMaxZoomed = new Vector3(6f, 6f, 6f);
     private Vector3 camMinZoomed = new Vector3(0.5f, 0.5f, 0.5f);
     private Vector3 camPos;
+
+    // camera rotation values
+    private float cameraRotOffset; // angle for camera
+    [SerializeField] float rotYAngle = 40f;
+    [SerializeField] float rotXAngle = 180f;
+    [SerializeField] float rotSpeed = 3f;
+
+    [SerializeField] Vector3 camOffset = new Vector3 (0f, 0.5f, -8f);
 
     // settings
     public bool orthographic = true; // set camera as orthographic or perspective
@@ -35,36 +45,44 @@ public class CameraZoom : MonoBehaviour
     {
         camPos = this.transform.position; // read out camera position
         Camera.main.orthographic = this.orthographic;
+        cameraRotOffset = rotXAngle;
     }
 
 
-    void Update()
+    void LateUpdate()
     {
-        // Player Setup and null checks
-        Vector3 boardCenter = (boardCenterPosition != null) ? boardCenterPosition.position : Vector3.zero;
-        Vector3 playerPos = (PhotonPlayerController.localPlayer != null) ? PhotonPlayerController.localPlayer.transform.position : boardCenter;
-        
-        // Zoom 
+
+        updateCameraZoom();
+        updateCameraRotation();
+    }
+
+    private void updateCameraZoom(){
         float currentMouseZoom = Input.mouseScrollDelta.y; // read out mouse scroll data 
         currentMouseZoom = Mathf.Clamp(currentMouseZoom, -scrollClamp, scrollClamp); // clamp data 
 
         zoomLerp = Mathf.Clamp(zoomLerp + currentMouseZoom * zoomSpeed, 0f, 1f);
-        Vector3 targetPos = Vector3.Lerp(playerPos, boardCenter, zoomLerp);
-        cameraGroundPos.position = Vector3.Lerp(cameraGroundPos.position, targetPos, Time.deltaTime * zoomSmoothing);
-        
 
-        if (!orthographic)
-        {
-            //camera for perspective view
-            camPos += new Vector3(currentMouseZoom, currentMouseZoom, currentMouseZoom);
-            camPos = new Vector3(Mathf.Clamp(camPos.x, camMinZoomed.x, camMaxZoomed.x), Mathf.Clamp(camPos.y, camMinZoomed.y, camMaxZoomed.y), Mathf.Clamp(camPos.z, camMinZoomed.z, camMaxZoomed.z));
-            transform.position = Vector3.Lerp(transform.position, camPos, Time.deltaTime * 10f);
-        }
-        else
-        {
-            //camera for orthographic view
-            float zoomSize = Mathf.Lerp(orthographicMinSize, orthographicMaxSize, zoomLerp);
-            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomSize, Time.deltaTime * zoomSmoothing);
-        }
+        float zoomSize = Mathf.Lerp(orthographicMinSize, orthographicMaxSize, zoomLerp);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, zoomSize, Time.deltaTime * zoomSmoothing);
     }
+
+    private void updateCameraRotation()
+    {
+        Vector3 playerPos = getPlayerPos();
+
+        Vector3 mouseDelta = Input.mousePosition - lastMousePos;
+        lastMousePos = Input.mousePosition;
+
+        cameraRotOffset += mouseDelta.x * rotSpeed;
+
+        Quaternion rotation = Quaternion.Euler(rotYAngle, cameraRotOffset, 0f);
+        Camera.main.transform.rotation = rotation;
+
+        Vector3 position = rotation * camOffset + playerPos;
+        Camera.main.transform.position = position;
+    }
+ 
+    private Vector3 getPlayerPos() => (PhotonPlayerController.localPlayer != null) ? PhotonPlayerController.localPlayer.transform.position : getBoardCenter();
+    private Vector3 getBoardCenter () => (boardCenterPosition != null) ? boardCenterPosition.position : Vector3.zero;
+
 }
