95f3a6730470f5de8278221f0e36e4670b9be446
PleekMan <agusanzorena@gmail.com>
2020-07-08 20:56:50 +0200

    dev: new: Implementing reading JSON for LCD_Screen

--- Assets/Scripts/LCD_ScreenController.cs
+++ Assets/Scripts/LCD_ScreenController.cs
@@ -2,73 +2,90 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
-public class LCD_ScreenController : MonoBehaviour {
-
- public TMP_Text LCDText;
- public float scrollSpeed = 1.0f;
- private string originalText;
-
- private TMP_Text cloneText;
- private RectTransform textRectTransform;
- private string sourceText;
- private string tempText;
+using System.IO;
 
 
+public class LCD_ScreenController : MonoBehaviour
+{
 
- void Start()
- {
-   Debug.Log("In this shit");
-   LCDText = GameObject.Find("LCD_Text_TMP").GetComponent<TMP_Text>();
-   originalText = LCDText.text;
-   textRectTransform = LCDText.GetComponent<RectTransform>();
+    public TMP_Text LCDText;
+    public float scrollSpeed = 1.0f;
+    private string originalText;
 
-   //LCDText.text = "Fuuuu";
-   // cloneText = Instantiate(LCDText) as TMP_Text;
-   //
-   // RectTransform cloneRectTransform = cloneText.GetComponent<RectTransform>();
-   // cloneRectTransform.SetParent(textRectTransform);
-   // cloneRectTransform.anchorMin = new Vector2(1, 0.5f);
-   // cloneRectTransform.localPosition = new Vector3(LCDText.preferredWidth, 0, cloneRectTransform.position.z);
-   // cloneRectTransform.localScale = new Vector3(1, 1, 1);
-   // cloneText.text = LCDText.text;
+    private TMP_Text cloneText;
+    private RectTransform textRectTransform;
+    private string sourceText;
+    private string tempText;
+
+    [SerializeField] LcdData lcdDataEditor;
+
+    static readonly string serverPath = "http://134.122.74.56/space/appdata/"; // use path to save data on virtual machine
+    //static string credentialsLocalPath() => Application.dataPath + "/Resources/" + "lcdDatabase.json";
+    static string credentialsServerPath() => serverPath + "lcdDatabase.json"; // to put hard coded server path
 
 
-    //StartCoroutine("Scroll");
-    InvokeRepeating("Scroll", 2.0f, 0.3f);
-   Debug.Log("In this Done");
+    [System.Serializable]
+    public class LcdData
+    {
+        [System.Serializable]
+        public class LcdDescription
+        {
+            public string text;
+        }
+        public LcdDescription pinDescriptions;
 
+
+    }
 
 
 
- }
-
- void Scroll()
- {
-
-   float width = LCDText.preferredWidth;
- //  Debug.Log("FFFFFFFF");
- //  Debug.Log(string.Format ("Scrolling {0}", width));
-   Vector3 startPosition = textRectTransform.localPosition;
-
-   float scrollPosition = 0;
-   //string text = LCDText.text.Substring(1);
-   string text = LCDText.text;
+    void Start()
+    {
+        LCDText = GameObject.Find("LCD_Text_TMP").GetComponent<TMP_Text>();
+        originalText = LCDText.text;
+        textRectTransform = LCDText.GetComponent<RectTransform>();
 
 
+        //StartCoroutine("Scroll");
+        InvokeRepeating("Scroll", 5.0f, 0.3f);
+        InvokeRepeating("UpdateJsonFromServer", 1.0f, 10.0f);
+        //Debug.Log("In this Done");
 
-   LCDText.text = text.Substring(1,text.Length-1) + text.Substring(0, 1);;
+    }
+
+    private void Update()
+    {
+        //LCDText.text = TExtDelJSON; // TODO
+    }
+
+    void Scroll()
+    {
+
+        float width = LCDText.preferredWidth;
+
+        Vector3 startPosition = textRectTransform.localPosition;
+        float scrollPosition = 0;
+        string text = LCDText.text;
 
 
 
-   // while (true)
-   // {
-   //  //textRectTransform.localPosition = new Vector3(-scrollPosition % width, startPosition.y, startPosition.z);
-   //  String text =  LCDText.text;
-   //  scrollPosition += scrollSpeed * 20 * Time.deltaTime;
-   //  if (Time.deltaTime )
-   //  //Debug.Log(string.Format ("{0}scrollPosition: {}", scrollPosition));
-   //  yield return null;
-   // }
- }
+        LCDText.text = text.Substring(1, text.Length - 1) + text.Substring(0, 1); ;
+
+    }
+
+    void UpdateJsonFromServer()
+    {
+        readDataFromFile();
+    }
+
+
+    private LcdData readDataFromFile()
+    {
+        StreamReader reader = new StreamReader(credentialsServerPath());
+        string json = reader.ReadToEnd();
+        reader.Close();
+        //Debug.Log("json: " + json);
+        return JsonUtility.FromJson<LcdData>(json);
+
+    }
 }
