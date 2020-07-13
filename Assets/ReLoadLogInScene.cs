a961e7e6f8d886146ee1ea08e42f31cabc73fe8b
Isabel Paehr <isabel.paehr@gmail.com>
2020-07-13 15:53:06 +0200

    dev: chg: pretty LogIn UI

---
+++ Assets/ReLoadLogInScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReLoadLogInScene : MonoBehaviour
+{
+    public void ReLoad() {
+        SceneManager.LoadScene(0);
+      
+    }
+ 
+}
